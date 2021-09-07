19d31bb8132028d8240bc92cb6fbbdf18205bad0
elliottdombrowski <elliottroyal@me.com>
2021-09-07 03:10:08 -0500

    Fixed appends and added for loop to iterate through, instead of appending individually. Added functional local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var APIKey = "08fd305a5d7db027100d162178b62865";
 var city;
 
+var header = $("#city-display");
 var forecast = $("#forecast-wrapper");
 var windDisplay = $("#wind");
 var tempDisplay = $("#temp");
@@ -12,57 +13,120 @@ var dayTwo = $("#forecast-2");
 var dayThree = $("#forecast-3");
 var dayFour = $("#forecast-4");
 var dayFive = $("#forecast-5");
-// var dayCast = $("");
 
 var input = $("#city-input");
 var searchButton = $("#city-input-button");
 
+var res;
 
-searchButton.on("click", appendCity);
+//Search button event listener
+searchButton.on("click", handleSubmit);
 
-//APPENDS RECENT SEARCHES
-function appendCity() {
-    var cityDisplay = $("#city-display-table");
-    var tableEl = $('<button>')
-    
-    cityDisplay.append(tableEl);
-    tableEl.text(input.val())
 
-    //Calls saveCity to save input to local storage
-    saveCity()
-    //Calls callCity to pull and append API data
+//Called on button click, calls input city to display
+function handleSubmit() {
     callCity()
 }
 
+//Appends recent searches
+function appendCity(cityName) {
+    //Declares variables to append
+    var cityDisplay = $("#city-display-table");
+    var tableEl = $('<button>')
 
-//saveCity to store persistent local data
-function saveCity() {
-    localStorage.setItem("cities", input.val());
-    console.log("save working?");
+    //On recent searches/appended button click, calls respective city
+    tableEl.on("click", function() {
+        input.val(cityName);
+        callCity();
+    });
+    
+    //Prepends, so most recent search remains at top of container
+    cityDisplay.prepend(tableEl);
+    tableEl.text(cityName);
 }
 
+//Pulls local storage
+function readCities() {
+    var cityListStr = localStorage.getItem("cityList");
+
+    //Creates empty string for local storage / city searches
+    var cityList = [];
+
+    if (cityListStr !== null) {
+        cityList = JSON.parse(cityListStr);
+    }
+
+    return cityList;
+}
+
+function saveCity(cityName) {
+    var cityList = readCities();
+
+    //If user input is already in cityList string, console log response
+    if (cityList.indexOf(cityName) !== -1) {
+        console.log("already exists");
+    } else {
+        //Else, add user input to cityList array
+        cityList.push(cityName);
+        appendCity(cityName);
+    }
+
+    localStorage.setItem("cityList", JSON.stringify(cityList));
+}
+
+//Calls data from API
 function callCity() {
-    city = "chicago";
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
-    console.log(url);
+    //Forces all user input to uppercase, for uniformity on the page
+    var city = input.val().toUpperCase();
+
+    //FIX THIS
+    //FIX THIS
+    //FIX THIS
+    //FIX THIS
+    //FIX THIS
+    //FIX THIS
+    var queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${APIKey}`
 
     $.ajax({
-        url: url,
-        type: "GET",
-        success: function (result) {
-            showForecast(result)
-        },
-        error: function (error) {
-            console.log(error);
-        }
-    })  
+        //Pulls URL from API
+        url: queryURL,
+        method: "GET",
+    }).then(function(response){
+        //Take city search parameters and delcare variables for lat/long
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
+
+        $.ajax({
+            //Then, pull new API URL with lat/lon of chosen city
+            url: `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&appid=${APIKey}`
+        }).then(function(data) {
+
+            //Loop through array, and increment through API's daily array
+            //Will append current data for city to main containers, and for each consecutive day to respective containers.
+            for (var i = 0; i < 6; i++){
+                $("#weather-icon" + i).attr("src", "http://openweathermap.org/img/wn/" + data.daily[i].weather[0]);
+                $("#temp" + i).text("Temp: " + data.daily[i].temp.day);
+                $("#wind" + i).text("Wind: " + data.daily[i].wind_speed);
+                $("#humidity" + i).text("Humidity: " + data.daily[i].humidity);
+                $("#uv").text("UV Index: " + data.daily[i].uvi);
+            }
+
+            saveCity(city)
+        })
+    })
 }
 
-function showForecast(forecast) {
-    var temp = forecast.main;
-    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`
+//Appends local storage
+function getCity() {
+    var cityList = readCities();
+    
+    //Loops through array of appended city searches and appends list
+    for (i = 0; i < cityList.length; i++) {
+        appendCity(cityList[i]);
+        console.log(cityList[i]);
+    }
 
-
-    tempDisplay.textContent = temp;
-    console.log(forecast)
 }
+
+//Pulls local storage data
+getCity()
